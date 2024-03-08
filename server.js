4324d8ad83d7c42292514852a4d2dac55fb661a3
x-x00 <kaannalbayrak@gmail.com>
2024-03-08 11:57:53 +0300

    sending info via ejs and getting info via body-parser

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const mysql = require('mysql2');
 const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -16,11 +23,25 @@ connection.connect((err) => {
     else console.log("connected to database successfully.");
 });
 
-app.get('/', function (req, res) {
-    connection.query({sql: "SELECT * FROM sehirler"}, (err, results) => {
+// app.get('/', function (req, res) {
+//     connection.query({sql: "SELECT * FROM sehirler"}, (err, results) => {
+//         if(err) res.send(err);
+//         else res.send(results);
+//     });
+//   });
+
+app.route('/').get((req, res) =>{
+    connection.query({sql: "SELECT * FROM gonderiler"}, (err, results) => {
         if(err) res.send(err);
-        else res.send(results);
+        else res.render("main", {test: results});
     });
-  });
+}).post((req, res) => {
+    const newCityCode = parseInt(req.body.newCityCode);
+    const newCity = String(req.body.newCity);
+    connection.query({sql: `INSERT INTO sehirler (sehir_kodu, sehir_adi) VALUES (${newCityCode}, '${newCity}')`}, (err, results) => {
+        if(err) res.send(err);
+        else console.log('saves to sehirler table on mydb2.');
+    });
+});
   
 app.listen(3000);
