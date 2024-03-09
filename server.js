ec875fe01a4766526089c89764208c8089db4831
x-x00 <kaannalbayrak@gmail.com>
2024-03-09 14:54:49 +0300

    added some templates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -23,25 +23,39 @@ connection.connect((err) => {
     else console.log("connected to database successfully.");
 });
 
-// app.get('/', function (req, res) {
-//     connection.query({sql: "SELECT * FROM sehirler"}, (err, results) => {
-//         if(err) res.send(err);
-//         else res.send(results);
-//     });
-//   });
-
-app.route('/').get((req, res) =>{
-    connection.query({sql: "SELECT * FROM gonderiler"}, (err, results) => {
-        if(err) res.send(err);
-        else res.render("main", {test: results});
-    });
-}).post((req, res) => {
-    const newCityCode = parseInt(req.body.newCityCode);
-    const newCity = String(req.body.newCity);
-    connection.query({sql: `INSERT INTO sehirler (sehir_kodu, sehir_adi) VALUES (${newCityCode}, '${newCity}')`}, (err, results) => {
-        if(err) res.send(err);
-        else console.log('saves to sehirler table on mydb2.');
-    });
+app.route('/').get((req, res) => {
+    res.render('shop');
 });
+
+app.route('/cart').get((req, res) => {
+    res.render('cart');
+});
+
+app.route('/checkout').get((req, res) => {
+    res.render('checkout');
+});
+
+app.route('/thankyou').get((req, res) => {
+    res.render('thankyou');
+});
+
+
   
-app.listen(3000);
+app.listen(3000, '', (err) => {
+    if(err) console.log(err);
+    else console.log("Listening on port 3000.");
+});
+
+// app.route('/').get((req, res) =>{
+  //     connection.query({sql: "SELECT * FROM gonderiler"}, (err, results) => {
+  //         if(err) console.log(err);
+  //         else res.render("main", {test: results});
+  //     });
+  // }).post((req, res) => {
+  //     const newCityCode = parseInt(req.body.newCityCode);
+  //     const newCity = String(req.body.newCity);
+  //     connection.query({sql: `INSERT INTO sehirler (sehir_kodu, sehir_adi) VALUES (${newCityCode}, '${newCity}')`}, (err, results) => {
+  //         if(err) console.log(err);
+  //         else console.log('saved to sehirler table on mydb2.');
+  //     });
+  // });
